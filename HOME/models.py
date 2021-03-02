0e2b217b2aa61307cf8fe8165027545a400a2f08
mohitdmak <mdmohitrc@gmail.com>
2021-03-02 21:42:01 +0530

    documented views,models and removed try except method

--- HOME/models.py
+++ HOME/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from allauth.socialaccount.models import SocialAccount
 
-#Creating a through model for relating file objects with social account user objects
+#Creating a through model for relating FileObject model with SocialAccount user object.
 class Through(models.Model):
     ForUser = models.OneToOneField(SocialAccount, on_delete=models.CASCADE, related_name='filemodels')
     
+# Creating the FileObject model, which would contain the file name, and the File will be uploaded to 'media/documents/'
+# Also, the UserThrough ForeignKey property links the FileObject to the User who uploaded the file.
 class FileObject(models.Model):
     Name = models.CharField(max_length=300)
     File = models.FileField(upload_to='documents')
--- HOME/views.py
+++ HOME/views.py
@@ -1,34 +1,41 @@
 from django.shortcuts import redirect, render
 from allauth.socialaccount.models import SocialAccount
+from django.contrib.auth.models import User
 from .forms import FileForm
 from .models import Through
 
+#function-based view to direct the user to the home page
 def home(request):
-    if request.user.id:
+    # If user is logged in, we need the socialaccount object to display or post the form to upload files,
+    # since the FileObject model is connected with the SocialAccount via Through model. 
+    if request.user.is_authenticated:
         djangouser = request.user
         socialaccuser = SocialAccount.objects.filter(user = djangouser)[0]
         if request.method == 'POST':
+            # Adding a 'request.FILES' parameter to capture the file which the user had uploaded.
             form = FileForm(request.POST, request.FILES)
+            # Checking if the SocialAccount User already has a Through model created for FileObject,
+            # If created, directly linking it with the form instance, else creating the Through model first, and then linking it.
+            # After the File is saved to the database, redirecting the user to the home page.
             if form.is_valid():
-                try:
-                    socialaccuser.filemodels
-                except:
-                    Through.objects.create(ForUser = socialaccuser)
-                else:
-                    pass
-                finally:
+                if Through.objects.filter(ForUser = socialaccuser).exists():
                     form.instance.UserThrough = socialaccuser.filemodels
                     form.save()
                     return redirect('home')
+                else:
+                    Through.objects.create(ForUser = socialaccuser)
+                    form.instance.UserThrough = socialaccuser.filemodels
+                    form.save()
+                    return redirect('home')
+        # If get request, we still need to ensure that the Through model exists for the logged in user as we need it to,
+        # access the files that the user has uploaded and display them
         else:
-            try:
-                socialaccuser.filemodels
-            except:
-                Through.objects.create(ForUser = socialaccuser)
-            else:
-                pass
-            finally:
+            if Through.objects.filter(ForUser = socialaccuser).exists():
                 return render(request, 'home.html', {'f_form': FileForm, 'files':socialaccuser.filemodels.files.all()})
+            else:
+                Through.objects.create(ForUser = socialaccuser)
+                return render(request, 'home.html', {'f_form': FileForm, 'files':socialaccuser.filemodels.files.all()})
+    # In this case, the user is logged out, and thus he would not see the File Uploading Form or Files uploaded
+    # Here the user can only access the logging in button.
     else:
         return render(request, 'home.html')
-
